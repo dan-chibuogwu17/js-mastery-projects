94d398ee7669c8a7bfee29c2c22b9110af1fd1e0
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-09-29 23:32:16 +0100

    just saving but working on the closer look at functions section

--- Functions/script.js
+++ Functions/script.js
@@ -1,28 +1,74 @@
 'use strict';
 
 
-const secureBooking = function() {
-  let passengerCount = 0;
 
-  return function() {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  let num = 1;
+ const exp = function () {
+   header.style.color = 'blue';
+   console.log(num++);
+   console.dir(exp);
+ };
+ document.querySelector('body').addEventListener('click', exp );
+
+})();
+
+
+{
+  function danielIIFE() {
+    console.log("IIFE running");
   }
+  danielIIFE();
 }
 
-const booker = secureBooking();
-
-booker();
-booker();
-booker();
-
-console.dir(booker);
+danielIIFE();
 
 
-const num = "18";
-console.log(num.padStart(2, '0'));
 
 
+
+
+
+
+
+// const secureBooking = function() {
+//   let passengerCount = 0;
+//
+//   return function() {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   }
+// }
+//
+// const booker = secureBooking();
+//
+// booker();
+// booker();
+// booker();
+//
+// console.dir(booker);
+//
+//
+// const num = "18";
+// console.log(num.padStart(2, '0'));
+//
+//
 
 
 
